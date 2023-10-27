d5cbd7afebf51aee82b9b106697232dd08e27c47
Peter Müllner <pmuellner@know-center.at>
2023-10-27 10:59:41 +0200

    hyperparameter tuning

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,20 +1,23 @@
 from recbole.trainer import HyperTuning
 from recbole.quick_start import objective_function
-import numpy as np
 import argparse
 from datetime import datetime as dt
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", default="ml-100k_custom")
+parser.add_argument("--dataset")
 parser.add_argument("--model_name")
 parser.add_argument("--params")
 parser.add_argument("--config")
 args = parser.parse_args()
 
+# path to the file containing the parameter space
 params_file = "hp/" + args.params + ".hyper"
+# path to the file containing the config of the model (which specifies the dataset, GPU, ...)
 config_file = "hp/" + args.config + ".yaml"
+# where to save the output file
 output_file = "hp/" + args.dataset + "/" + args.model_name + ".results"
 
+# Perform gridsearch
 starttime = dt.now()
 hp = HyperTuning(objective_function=objective_function, algo='exhaustive', early_stop=int(1e6),
                  params_file=params_file, fixed_config_file_list=[config_file])
